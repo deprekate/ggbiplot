b7a4753dd5c1196809942c7e3f4345118c134d1f
deprekate <deprekate@gmail.com>
2020-02-18 22:06:38 -0800

    Replacing proto with ggproto

--- R/geom-circle.R
+++ R/geom-circle.R
@@ -1,58 +1,59 @@
-GeomCircle <- ggproto(ggplot2:::Geom, expr={
-  objname <- "circle"
+GeomCircle <- ggproto("GeomCircle", Geom,
+	#objname <- "circle"
+	default_stat = function(.) StatIdentity,
+	default_pos = function(.) PositionIdentity,
+	default_aes = function(.) aes(
+		colour = "black", 
+		size = 0.5, 
+		linetype = 1, 
+		fill = NA, 
+		alpha = NA
+	),
+	required_aes = c("x", "y", "radius"),
+	guide_geom = function(.) "polygon",
+	draw_panel = function(data, scales, coordinates, ...) {
+		nsegment <- 51
+		if (empty(data)) return(zeroGrob())
 
-  default_stat <- function(.) StatIdentity
-  default_pos <- function(.) PositionIdentity
-  default_aes <- function(.) aes(
-    colour = "black", 
-    size = 0.5, 
-    linetype = 1, 
-    fill = NA, 
-    alpha = NA
-  )
-  required_aes <- c("x", "y", "radius")
+		#if (!is.linear(coordinates)) {
+		#  warning("geom_circle does not work properly with non-linear coordinates.")
+		#}
 
-  guide_geom <- function(.) "polygon"
+		data <- unique(data)
 
-  draw <- function(., data, scales, coordinates, ...) {
-    nsegment <- 51
-    if (empty(data)) return(zeroGrob())
-
-    if (!is.linear(coordinates)) {
-      warning("geom_circle does not work properly with non-linear coordinates.")
-    }
-
-    data <- unique(data)
-
-    circle <- data.frame(x = cospi(seq(0, 2, length.out = nsegment)),
+		circle <- data.frame(x = cospi(seq(0, 2, length.out = nsegment)),
                          y = sinpi(seq(0, 2, length.out = nsegment)))
 
-    grobs <- lapply(seq_len(nrow(data)), function(i) {
-      df <- transform(data[rep(i, nrow(circle)), ],
+		grobs <- lapply(seq_len(nrow(data)), function(i) {
+			df <- transform(data[rep(i, nrow(circle)), ],
                       x = x + radius * circle$x, 
                       y = y + radius * circle$y,
                       radius = NULL)
-      with(coord_transform(coordinates, df, scales),
-        polygonGrob(x, y, default.units = "native",
-          gp = gpar(col = alpha(colour, alpha),
-                    fill = fill,
-                    lwd = size * .pt,
-                    lty = linetype)
-        )
-      )
-    })
+			with(coord_transform(coordinates, df, scales),
+				polygonGrob(x, y, default.units = "native",
+					gp = gpar(col = alpha(colour, alpha),
+                    	fill = fill,
+                    	lwd = size * .pt,
+                    	lty = linetype)
+				)
+			)
+    	})
 
-    do.call("gList", grobs)
-  }
-})
+		do.call("gList", grobs)
+  	}
+)
 
 #' Circles specified by position and radius
 #'
 #' @param arrow specification for arrow heads, as created by arrow()
 #' @export
-geom_circle <- function (mapping = NULL, data = NULL, stat = "identity",
-  position = "identity", ...) {
-
-  GeomCircle$new(mapping = mapping, data = data, stat = stat,
+geom_circle <- function (mapping = NULL, data = NULL, stat = "identity", position = "identity", ...) {
+	layer(
+        geom = GeomCircle, mapping = mapping,
+        data = data, stat = stat, position = position,
+        ...
+	)
+	
+	GeomCircle$new(mapping = mapping, data = data, stat = stat,
     position = position, ...)
 }

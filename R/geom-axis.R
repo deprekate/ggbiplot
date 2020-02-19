52984fcb8c00410ba08646bad30019f8130c91bd
deprekate <deprekate@gmail.com>
2020-02-18 21:54:59 -0800

    Replacing proto with ggproto

--- R/geom-axis.R
+++ R/geom-axis.R
@@ -1,58 +1,55 @@
-GeomAxis <- ggproto(ggplot2:::Geom, expr={
-  objname <- "axis"
+GeomAxis <- ggproto("GeomAxis", Geom,
+	required_aes = c("x", "y"),
+	default_aes = aes(
+		xbegin = 0, 
+		ybegin = 0, 
+		colour = "#832424FF", 
+		label = NULL, 
+		size = 0.5, 
+		linetype = 1, 
+		alpha = NA,
+		textsize = 3,
+		family = "",
+		fontface = 1,
+		lineheight = 1.2
+	),
+	extra_params = c("na.rm", "garrow"),
+	draw_panel = function(data, panel_params, coord, garrow) {
+		if (empty(data)) return(zeroGrob())
+		#if (!is.linear(coord)) {
+		#	warning("geom_axis does not work properly with non-linear coordinates.")
+		#}
+		
+		vec <- transform(data,textsize = NULL, family = NULL, fontface = NULL, lineheight = NULL)
 
-  default_stat <- function(.) StatIdentity
-  required_aes <- c("x", "y")
-  default_aes <- function(.) aes(
-    xbegin = 0, 
-    ybegin = 0, 
-    colour = "#832424FF", 
-    label = NULL, 
-    size = 0.5, 
-    linetype = 1, 
-    alpha = NA,
-    textsize = 3,
-    family = "",
-    fontface = 1,
-    lineheight = 1.2
-  )
-  guide_geom <- function(.) "segment"
-
-  draw <- function(., data, scales, coordinates, arrow, ...) {
-
-    if (empty(data)) return(zeroGrob())
-
-    if (!is.linear(coordinates)) {
-      warning("geom_axis does not work properly with non-linear coordinates.")
-    }
-
-    vec <- transform(data, 
-      textsize = NULL, family = NULL, fontface = NULL, lineheight = NULL
-    )
-
-    gList(
-      if (!is.null(data$label)) {
-        text <- transform(data, 
-          size = textsize,
-          angle = (180/pi) * atan(y / x),
-          hjust = 0.5 * (1 - 1.25 * sign(x)),
-          vjust = 0.5
-        )
-        GeomText$draw(text, scales, coordinates)
-      },
-      ggbiplot2:::GeomVector$draw(vec, scales, coordinates, arrow = arrow)
-    )
-  }
-})
+		gList(
+			if (!is.null(data$label)) {
+				text <- transform(data, 
+					size = textsize,
+					angle = (180/pi) * atan(y / x),
+					hjust = 0.5 * (1 - 1.25 * sign(x)),
+					vjust = 0.5
+				)
+				GeomText$draw_panel(text, panel_params, coord)				
+			},
+			ggbiplot2:::GeomVector$draw_panel(vec, panel_params, coord, garrow)
+    	)
+  	}
+)
 
 #' Axis arrows with optional text labels.
 #'
 #' @param arrow specification for arrow heads, as created by arrow()
 #' @export
-geom_axis <- function (mapping = NULL, data = NULL, stat = "identity",
-  position = "identity", arrow = grid::arrow(length = unit(1/3, "picas")), 
+pca_axis <- function (mapping = NULL, data = NULL, stat = "identity",
+  position = "identity", na.rm = FALSE, garrow = grid::arrow(length = unit(1/3, "picas")), 
   ...) {
-
-  GeomAxis$new(mapping = mapping, data = data, stat = stat,
-    position = position, arrow = arrow, ...)
+      layer(
+        geom = GeomAxis, mapping = mapping,
+        data = data, stat = stat, position = position,
+        #arrow = arrow,
+        #show.legend = show.legend, inherit.aes = inherit.aes,
+        params = list(na.rm = na.rm, garrow = garrow, ...),
+        ...
+      )
 }
--- R/geom-vector.R
+++ R/geom-vector.R
@@ -1,23 +1,20 @@
-GeomVector <- ggproto(ggplot2:::Geom, expr={
-  objname <- "vector"
-
-  default_stat <- function(.) StatIdentity
-  required_aes <- c("x", "y")
-  default_aes <- function(.) aes(
+GeomVector <- ggproto("GeomVector", Geom,
+  #objname <- "vector"
+  #default_stat <- function(.) StatIdentity
+  required_aes = c("x", "y"),
+  default_aes = function(.) aes(
     xbegin = 0, 
     ybegin = 0, 
     colour = "black", 
     size = 0.5, 
     linetype = 1, 
     alpha = NA
-  )
-  guide_geom <- function(.) "segment"
-
-  draw <- function(., data, scales, coordinates, 
-    arrow = grid::arrow(length = unit(1/3, "picas")), ...) {
-
+  ),
+  #guide_geom = function(.) "segment"
+  extra_params = c("na.rm", "garrow"),
+  draw_panel = function(data, scales, coordinates, garrow) {
     if (empty(data)) return(zeroGrob())
-
+	
     # Drop rows where (x, y) is nearly equal to (xbegin, ybegin)
     r2 <- with(data, (x - xbegin)^2 + (y - ybegin)^2)
     data <- subset(data, Vectorize(all.equal)(r2, 0) != TRUE)
@@ -26,18 +23,22 @@ GeomVector <- ggproto(ggplot2:::Geom, expr={
       xend = x, yend = y, x = xbegin, y = ybegin,
       xbegin = NULL, ybegin = NULL
     )
-
-    GeomSegment$draw(segment, scales, coordinates, arrow = arrow)
+    gList(
+    	GeomSegment$draw_panel(segment, scales, coordinates, arrow=garrow)
+    )
   }
-})
+)
 
 #' Arrows
 #'
 #' @param arrow specification for arrow heads, as created by arrow()
 #' @export
 geom_vector <- function (mapping = NULL, data = NULL, stat = "identity",
-  position = "identity", arrow = grid::arrow(length = unit(1/3, "picas")), ...) {
-
-  GeomVector$new(mapping = mapping, data = data, stat = stat,
-    position = position, arrow = arrow, ...)
+  position = "identity", na.rm = FALSE, garrow = grid::arrow(length = unit(1/3, "picas")), ...) {
+	layer(
+		geom = GeomVector, mapping = mapping,
+		data = data, stat = stat, position = position,
+		params = list(na.rm = na.rm, garrow = garrow),
+		...
+    )
 }
